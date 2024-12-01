af2c2fda5524d45e35e7e72c86627f1f5f0f5017
dain7 <dainnnnnn96@gmail.com>
2024-12-01 20:31:51 +0900

    first commit

---
+++ src/main/kotlin/com/kimsisters/saju8ja/Saju8jaApplication.kt
@@ -0,0 +1,11 @@
+package com.kimsisters.saju8ja
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Saju8jaApplication
+
+fun main(args: Array<String>) {
+	runApplication<Saju8jaApplication>(*args)
+}
---
+++ src/test/kotlin/com/kimsisters/saju8ja/Saju8jaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kimsisters.saju8ja
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Saju8jaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
