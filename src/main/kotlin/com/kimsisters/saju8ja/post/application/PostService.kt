993bae3d73f42b9877c1abc97d6b82772f6b3849
dain7 <dainnnnnn96@gmail.com>
2024-12-15 14:19:51 +0900

    feat: annotation 정리

--- src/main/kotlin/com/kimsisters/saju8ja/post/application/PostService.kt
+++ src/main/kotlin/com/kimsisters/saju8ja/post/application/PostService.kt
@@ -8,10 +8,12 @@ import com.kimsisters.saju8ja.post.application.port.command.PostResponse
 import com.kimsisters.saju8ja.post.application.port.out.CreatePostPort
 import com.kimsisters.saju8ja.post.application.port.out.GetPostPort
 import com.kimsisters.saju8ja.post.domain.Post
+import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.ZoneOffset
 
+@Service
 class PostService(
     private val getCategoryPort: GetCategoryPort,
     private val createPort: CreatePostPort,
--- src/main/kotlin/com/kimsisters/saju8ja/post/application/port/in/PostUseCase.kt
+++ src/main/kotlin/com/kimsisters/saju8ja/post/application/port/in/PostUseCase.kt
@@ -2,9 +2,7 @@ package com.kimsisters.saju8ja.post.application.port.`in`
 
 import com.kimsisters.saju8ja.post.application.port.command.PostRequest
 import com.kimsisters.saju8ja.post.application.port.command.PostResponse
-import org.springframework.stereotype.Component
 
-@Component
 interface PostUseCase {
     fun create(command: PostRequest.CreatePost): PostResponse.Post
     fun getPosts(categoryId: String): PostResponse.GetPosts
